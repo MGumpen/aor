635c80e6172b7ab16a0e6397bbe9307ec441a49b
MGumpen <marius.gundersen@live.no>
2025-09-15 15:21:46 +0200

    Lagt til Registrer obstacle, DataForm, ObstacleData, ObstacleController, Overviev. Lagt inn Leaflet i wwwroot > lib

---
+++ AOR/Models/ObstacleData.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AOR.Models;
+
+public class ObstacleData
+{
+    [MaxLength(100)]
+    public string ObstacleName { get; set; }
+
+    [Required(ErrorMessage = "Field is required")]
+    [Range(0, 200)]
+    public double ObstacleHeight { get; set; }
+
+    [MaxLength(1000)]
+    public string ObstacleDescription { get; set; }
+
+    public bool IsDraft { get; set; }
+
+    public string? GeometryGeoJson { get; set; }
+}
