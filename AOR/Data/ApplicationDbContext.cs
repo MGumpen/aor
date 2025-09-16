381bccc0ae75929787aba8ffb33cfa52bedd33c4
MGumpen <marius.gundersen@live.no>
2025-09-16 11:32:22 +0200

    Lagt inn kode som gjør at appen kan kjøre i docker og med mariaDB

---
+++ AOR/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AOR.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // DbSets for your entities will go here
+    // Example: public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Entity configurations will go here
+        // Example:
+        // modelBuilder.Entity<User>(entity =>
+        // {
+        //     entity.HasKey(e => e.Id);
+        //     entity.Property(e => e.Email).IsRequired().HasMaxLength(255);
+        // });
+    }
+}
