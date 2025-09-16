8144691bfca94fd49363f530eb7f5c1a3f9bddea
MGumpen <marius.gundersen@live.no>
2025-09-16 12:17:42 +0200

    Legger til og endrer innhold i ApplicationDbContext og AdviceDto

--- AOR/Data/ApplicationDbContext.cs
+++ AOR/Data/ApplicationDbContext.cs
@@ -7,21 +7,31 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+
     }
 
     // DbSets for your entities will go here
     // Example: public DbSet<User> Users { get; set; }
-
+    public DbSet<AdviceDto> Advices { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
-
-        // Entity configurations will go here
-        // Example:
-        // modelBuilder.Entity<User>(entity =>
-        // {
-        //     entity.HasKey(e => e.Id);
-        //     entity.Property(e => e.Email).IsRequired().HasMaxLength(255);
-        // });
+        modelBuilder.Entity<AdviceDto>().HasKey(keyId => keyId.adviceId);
+        {
+            
+        };
     }
+
+  //  protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //  base.OnModelCreating(modelBuilder);
+
+    // Entity configurations will go here
+    // Example:
+    // modelBuilder.Entity<User>(entity =>
+    // {
+    //     entity.HasKey(e => e.Id);
+    //     entity.Property(e => e.Email).IsRequired().HasMaxLength(255);
+    // });
+    // }
 }
