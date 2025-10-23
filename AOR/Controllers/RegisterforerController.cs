b93a292c2f0597bd08f57c0ce2c1239abf38c88a
MGumpen <marius.gundersen@live.no>
2025-10-23 14:51:23 +0200

    Fikset steder hvor navn på AorDbContext ikke var endret

--- AOR/Controllers/RegisterforerController.cs
+++ AOR/Controllers/RegisterforerController.cs
@@ -47,6 +47,6 @@ return View();
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 public IActionResult Error()
 {
-return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
 }
