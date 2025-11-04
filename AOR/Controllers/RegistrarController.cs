6bf4374a9cae60613d80b4bd7dff117df9d1a57b
MGumpen <marius.gundersen@live.no>
2025-11-04 21:36:50 +0100

    Endringer på reg siden

--- AOR/Controllers/RegistrarController.cs
+++ AOR/Controllers/RegistrarController.cs
@@ -4,18 +4,23 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using AOR.Models;
 using AOR.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace AOR.Controllers;
 [Authorize(Roles = "Registrar")]
 public class RegistrarController : Controller
 {
+    private readonly AorDbContext _db;
     private readonly ILogger<RegistrarController> _logger;
     private readonly AorDbContext _context;
+    private readonly UserManager<User> _userManager;
 
-    public RegistrarController(ILogger<RegistrarController> logger, AorDbContext context)
+    public RegistrarController(AorDbContext db, ILogger<RegistrarController> logger, AorDbContext context, UserManager<User> userManager)
     {
         _logger = logger;
         _context = context;
+        _db = db;
+        _userManager = userManager;
     }
 
     public IActionResult LogIn()
@@ -96,5 +101,27 @@ public class RegistrarController : Controller
     public IActionResult Error()
     {
         return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> ReportDetails(int id)
+    {
+        var userId = _userManager.GetUserId(User);
+        if (string.IsNullOrEmpty(userId))
+        {
+            return RedirectToAction("Index", "LogIn");
+        }
+
+        var report = await _db.Reports
+            .AsNoTracking()
+            .Where(r => r.UserId == userId && r.ReportId == id)
+            .Include(r => r.Obstacle)
+            .Include(r => r.Status)
+            .Include(r => r.User)
+            .FirstOrDefaultAsync();
+
+        ViewBag.DisplayName = User?.Identity?.Name ?? "User";
+
+        return View("ReportDetails", report);
     }
 }
